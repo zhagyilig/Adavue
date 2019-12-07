aa80453b2a1cee11312e687373b8e5d6529efe3a
zhagyilig <zhagyilig@gmail.com>
2019-12-07 13:32:00 +0800

    [Add] vue project.

---
+++ code/vue_shop/src/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <div id="app">
+    ericzhang
+  </div>
+</template>
+
+<style>
+
+</style>
---
+++ code/vue_shop/src/components/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    登陆组件
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+
+<style lang="less">
+
+</style>
